d29d69c704f03cc0787e5e78a42e998697eae539
DmitriiSasov <s-dima29@yandex.ru>
2021-01-02 18:42:48 +0300

    Удалены лишние файлы

--- compiler/generate_code.cpp
+++
@@ -1,6 +0,0 @@
-#include "generate_code.h"
-
-void generateCode(const programS* const program)
-{
-
-}
--- compiler/generate_code.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "transform_root.h"
-void generateCode(const programS* const program);
